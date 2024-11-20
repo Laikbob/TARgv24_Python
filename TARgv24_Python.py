247626d4153ec739e2bf49c24fbfec5ba8bb8e7d
opilane <opilane@HP-CZC2349HV0>
2024-11-20 15:47:19 +0200

    Üleasnne 1

--- TARgv24_Python.py
+++ TARgv24_Python.py
@@ -1,1 +1,8 @@
+#Ülesanene 1 
 print("Tere tulemast!")
+nimi=input("Mis on sinu nimi? ")
+print("Tere tulemast! Tevitan sind ", nimi)
+print("Tere tulemast! Tevitan sind "+ nimi)
+vanus=int(input("Kui vana sa oled? "))
+print("Tere tulemast! Tervitan sind  "+nimi+"Sa oled",vanus,"aastat vana")
+print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastat vana")
