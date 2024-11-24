746afabeabbcc7f1eb38e00fe0c4cb1580338cb2
Andrei Lebedev <andreilebedev321@gmail.com>
2024-11-24 15:49:19 +0200

    Tund 1

--- TARgv24_Python.py
+++ TARgv24_Python.py
@@ -1,34 +1,34 @@
-from math import pi
-from msilib import PID_LASTPRINTED
-from random import *  #*-kõik funksionid
-from math import * #pi kasutaminseks
+# from math import pi
+# from msilib import PID_LASTPRINTED
+# from random import *  #*-kõik funksionid
+# from math import * #pi kasutaminseks
 
-# # #Ülesanene 1
-# # print("Tere tulemast!")
-# # nimi=input("Mis on sinu nimi? "). capitalize() #lower()-aaa, upper()-AAA,capitalize()-Aaa
-# # print("Tere tulemast! Tevitan sind ", nimi)
-# # print("Tere tulemast! Tevitan sind "+ nimi)
-# # vanus=int(input("Kui vana sa oled? "))
-# # print("Tere tulemast! Tervitan sind  "+nimi+"Sa oled",vanus,"aastat vana")
-# # print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastat vana")
+# # # #Ülesanene 1
+# # # print("Tere tulemast!")
+# # # nimi=input("Mis on sinu nimi? "). capitalize() #lower()-aaa, upper()-AAA,capitalize()-Aaa
+# # # print("Tere tulemast! Tevitan sind ", nimi)
+# # # print("Tere tulemast! Tevitan sind "+ nimi)
+# # # vanus=int(input("Kui vana sa oled? "))
+# # # print("Tere tulemast! Tervitan sind  "+nimi+"Sa oled",vanus,"aastat vana")
+# # # print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastat vana")
 
-# # # Ülesanne 2
-# # vanus = 18
-# # eesnimi = "Jaak"
-# # pikkus = 16.5
-# # kas_käib_koolis = True
-# # print(type(vanus),(eesnimi),(pikkus))
+# # # # Ülesanne 2
+# # # vanus = 18
+# # # eesnimi = "Jaak"
+# # # pikkus = 16.5
+# # # kas_käib_koolis = True
+# # # print(type(vanus),(eesnimi),(pikkus))
 
-# # #Ülesanne 3
-# # kokku=randint(1,1000)
-# # print(f"Kokku on {kokku} kommi")
-# # kommi=int(input("Mitu kommi sa tahad? "))
-# # kokku=kokku-kommi
-# # print(f"Jääk on {kokku} kommi")
+# # # #Ülesanne 3
+# # # kokku=randint(1,1000)
+# # # print(f"Kokku on {kokku} kommi")
+# # # kommi=int(input("Mitu kommi sa tahad? "))
+# # # kokku=kokku-kommi
+# # # print(f"Jääk on {kokku} kommi")
 
-# #Ülesanne 4
-print("Labimöödu leidmine ")
-#l-ümbbermõõt
-l=float(input("Ümbermööt: "))
-d=l/pi
-print(f"Läbimõõdu suurus on {round(d,2)}")
+# # #Ülesanne 4
+# print("Labimöödu leidmine ")
+# #l-ümbbermõõt
+# l=float(input("Ümbermööt: "))
+# d=l/pi
+# print(f"Läbimõõdu suurus on {round(d,2)}")
